16a4a669f8e5eaad1e8a5778331aaa21c2d8a3b5
Aryan <aryan.khanna@chicmic.co.in>
2023-02-23 21:41:43 +0530

    enum Functionality Done

--- assets/size.ts
+++ assets/size.ts
@@ -16,85 +16,105 @@ export class size extends Component {
   rect1: Node = null;
   @property({ type: Node })
   rect2: Node = null;
-  @property(Node)
+
+  // @property({ type: Enum(CROPDECIDER) })
+  // get EnumValue(): any {
+  //   return this.cropdcide;
+  // }
+  // set EnumValue(value: any) {
+  //   this.cropdcide = value;
+  //   if (this.cropdcide == CROPDECIDER.CROP) {
+  //     this.withCrop();
+  //   } else if (this.cropdcide == CROPDECIDER.NONCROP) {
+  //     this.wihoutCrop();
+  //   }
+  // }
+  @property({ type: Node })
   get frame(): Node {
     return this.rect2;
   }
   set frame(value: Node) {
     this.rect2 = value;
-    this.wihoutCrop();
+    this.imageCrop();
+    // this.rect2.setScale(1,1)
   }
 
-  setFrameSize() {
-    console.log("Okey we are her ");
-  }
-  start() {
-    // switch(this.cropdcide){
-    //   case CROPDECIDER
-    // }
-  }
-  wihoutCrop() {
+  start() {}
+  imageCrop() {
     switch (this.cropdcide) {
       case CROPDECIDER.NONE: {
         console.log("Node");
         break;
       }
+
       case CROPDECIDER.CROP: {
         console.log("crop");
+        var rec1_width = this.rect1.getComponent(UITransform).width;
+        var rect1_height = this.rect1.getComponent(UITransform).height;
+        var aspectR1 = rec1_width / rect1_height;
+        console.log("aspect ratio of rect1", aspectR1);
+
+        var rect2_width = this.rect2.getComponent(UITransform).width;
+        var rect2_height = this.rect2.getComponent(UITransform).height;
+        var aspectR2 = rect2_width / rect2_height;
+        console.log("aspect ratio of rect2", aspectR2);
+        if (aspectR1 > aspectR2) {
+          var decider = rec1_width / rect2_width;
+          this.rect2.setScale(
+            this.rect2.getScale().x * decider,
+            this.rect2.getScale().y * decider
+          );
+        } else {
+          var decider = rect1_height / rect2_height;
+          this.rect2.setScale(
+            this.rect2.getScale().x * decider,
+            this.rect2.getScale().y * decider
+          );
+        }
         break;
       }
-    }
-    var rec1_width = this.rect1.getComponent(UITransform).width;
-    var rect1_height = this.rect1.getComponent(UITransform).height;
-    console.log("aspect ratio of rect1", rec1_width / rect1_height);
+      case CROPDECIDER.NONCROP: {
+        var rec1_width = this.rect1.getComponent(UITransform).width;
+        var rect1_height = this.rect1.getComponent(UITransform).height;
+        console.log("aspect ratio of rect1", rec1_width / rect1_height);
 
-    var rect2_width = this.rect2.getComponent(UITransform).width;
-    var rect2_height = this.rect2.getComponent(UITransform).height;
-    console.log("aspect ratio of rect2", rect2_width / rect2_height);
-    var scaleofRect = this.rect2.getScale();
-    if (rect2_height > rect2_width) {
-      var aspectRatio = rect1_height / rect2_height;
-      this.rect2.setScale(aspectRatio, aspectRatio);
-      var newH = this.rect2.getComponent(UITransform).getBoundingBox().height;
-      var newW = this.rect2.getComponent(UITransform).getBoundingBox().width;
-      console.log("aspect ratio of after rect2", newW / newH);
-    } else if (rect2_height < rect2_width) {
-      var aspect = rec1_width / rect2_width;
-      this.rect2.setScale(aspect, aspect);
-      var newH = this.rect2.getComponent(UITransform).getBoundingBox().height;
-      var newW = this.rect2.getComponent(UITransform).getBoundingBox().width;
-      console.log("aspect ratio of after rect2", newW / newH);
-    } else {
-      var aspectratio = rect1_height / rect2_height;
-      this.rect2.setScale(aspectratio, aspectratio);
-      var newH = this.rect2.getComponent(UITransform).getBoundingBox().height;
-      var newW = this.rect2.getComponent(UITransform).getBoundingBox().width;
-      console.log("aspect ratio of after rect2", newW / newH);
-    }
-  }
-
-  withCrop() {
-    var rec1_width = this.rect1.getComponent(UITransform).width;
-    var rect1_height = this.rect1.getComponent(UITransform).height;
-    var aspectR1 = rec1_width / rect1_height;
-    console.log("aspect ratio of rect1", aspectR1);
-
-    var rect2_width = this.rect2.getComponent(UITransform).width;
-    var rect2_height = this.rect2.getComponent(UITransform).height;
-    var aspectR2 = rect2_width / rect2_height;
-    console.log("aspect ratio of rect2", aspectR2);
-    if (aspectR1 > aspectR2) {
-      var decider = rec1_width / rect2_width;
-      this.rect2.setScale(
-        this.rect2.getScale().x * decider,
-        this.rect2.getScale().y * decider
-      );
-    } else {
-      var decider = rect1_height / rect2_height;
-      this.rect2.setScale(
-        this.rect2.getScale().x * decider,
-        this.rect2.getScale().y * decider
-      );
+        var rect2_width = this.rect2.getComponent(UITransform).width;
+        var rect2_height = this.rect2.getComponent(UITransform).height;
+        console.log("aspect ratio of rect2", rect2_width / rect2_height);
+        var scaleofRect = this.rect2.getScale();
+        if (rect2_height > rect2_width) {
+          var aspectRatio = rect1_height / rect2_height;
+          this.rect2.setScale(aspectRatio, aspectRatio);
+          var newH = this.rect2
+            .getComponent(UITransform)
+            .getBoundingBox().height;
+          var newW = this.rect2
+            .getComponent(UITransform)
+            .getBoundingBox().width;
+          console.log("aspect ratio of after rect2", newW / newH);
+        } else if (rect2_height < rect2_width) {
+          var aspect = rec1_width / rect2_width;
+          this.rect2.setScale(aspect, aspect);
+          var newH = this.rect2
+            .getComponent(UITransform)
+            .getBoundingBox().height;
+          var newW = this.rect2
+            .getComponent(UITransform)
+            .getBoundingBox().width;
+          console.log("aspect ratio of after rect2", newW / newH);
+        } else {
+          var aspectratio = rect1_height / rect2_height;
+          this.rect2.setScale(aspectratio, aspectratio);
+          var newH = this.rect2
+            .getComponent(UITransform)
+            .getBoundingBox().height;
+          var newW = this.rect2
+            .getComponent(UITransform)
+            .getBoundingBox().width;
+          console.log("aspect ratio of after rect2", newW / newH);
+        }
+        break;
+      }
     }
   }
 
